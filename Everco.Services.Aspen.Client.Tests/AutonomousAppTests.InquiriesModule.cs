67854bb4e27e1b638ab7da694ec6f9f61a8c60f3
Daniel E. Montalvo Hra <34135389+danielmontalvoh@users.noreply.github.com>
2019-11-22 08:59:13 -0500

    Avance pruebas de consultas

--- Everco.Services.Aspen.Client.Tests/AutonomousAppTests.InquiriesModule.cs
+++ Everco.Services.Aspen.Client.Tests/AutonomousAppTests.InquiriesModule.cs
@@ -7,10 +7,14 @@
 // -----------------------------------------------------------------------
 namespace Everco.Services.Aspen.Client.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Everco.Services.Aspen.Client.Auth;
     using Everco.Services.Aspen.Client.Fluent;
     using Everco.Services.Aspen.Client.Tests.Assets;
     using Everco.Services.Aspen.Client.Tests.Identities;
+    using Everco.Services.Aspen.Entities;
     using NUnit.Framework;
 
     /// <summary>
@@ -28,10 +32,60 @@ namespace Everco.Services.Aspen.Client.Tests
         {
             IAutonomousApp client = this.GetAutonomousClient();
             IAppIdentity appIdentity = AutonomousAppIdentity.Master;
-            IUserIdentity userIdentity = UserIdentity.Master;
             SqlDataContext.SetAppSettingsKey(appIdentity.ApiKey, "Bifrost:ConnectionStringName", "RabbitMQ:Local");
             SqlDataContext.SetAppSettingsKey(appIdentity.ApiKey, "Bancor:ConnectionStringName", "RabbitMQ:Local");
-            Assert.DoesNotThrow(() => client.Inquiries.GetAccounts(userIdentity.DocType, userIdentity.DocNumber));
+
+            IUserIdentity userIdentity = UserIdentity.Master;
+            string docType = userIdentity.DocType;
+            string docNumber = userIdentity.DocNumber;
+
+            IList<AccountInfo> GetAccounts() => client.Inquiries.GetAccounts(docType, docNumber);
+            Assert.DoesNotThrow(() => GetAccounts());
+            CollectionAssert.IsNotEmpty(GetAccounts());
+        }
+
+        /// <summary>
+        /// Obttener las cuentas de un usuario que no existe produce una salida válida.
+        /// </summary>
+        [Test]
+        [Category("Modules.Inquiries")]
+        public void GetAccountsFromUnrecognizedUserRequestWorks()
+        {
+            IAutonomousApp client = this.GetAutonomousClient();
+            IAppIdentity appIdentity = AutonomousAppIdentity.Master;
+            SqlDataContext.SetAppSettingsKey(appIdentity.ApiKey, "Bifrost:ConnectionStringName", "RabbitMQ:Local");
+            SqlDataContext.SetAppSettingsKey(appIdentity.ApiKey, "Bancor:ConnectionStringName", "RabbitMQ:Local");
+
+            string fixedDocType = "CC";
+            string randomDocNumber = new Random().Next(1000000000, int.MaxValue).ToString();
+
+            IList<AccountInfo> GetAccounts() => client.Inquiries.GetAccounts(fixedDocType, randomDocNumber);
+            Assert.DoesNotThrow(() => GetAccounts());
+            CollectionAssert.IsEmpty(GetAccounts());
+        }
+
+        /// <summary>
+        /// Obttener los saldos de una cuenta asociada a un usuario produce una salida válida.
+        /// </summary>
+        [Test]
+        [Category("Modules.Inquiries")]
+        public void GetBalancesRequestWorks()
+        {
+            IAutonomousApp client = this.GetAutonomousClient();
+            IAppIdentity appIdentity = AutonomousAppIdentity.Master;
+            SqlDataContext.SetAppSettingsKey(appIdentity.ApiKey, "Bifrost:ConnectionStringName", "RabbitMQ:Local");
+            SqlDataContext.SetAppSettingsKey(appIdentity.ApiKey, "Bancor:ConnectionStringName", "RabbitMQ:Local");
+
+            IUserIdentity userIdentity = UserIdentity.Master;
+            string docType = userIdentity.DocType;
+            string docNumber = userIdentity.DocNumber;
+            
+            IList<AccountInfo> accounts = client.Inquiries.GetAccounts(docType, docNumber);
+            CollectionAssert.IsNotEmpty(accounts);
+            AccountInfo tupAccountInfo = accounts.FirstOrDefault(account => account.Source == Subsystem.Tup);
+            Assert.IsNotNull(tupAccountInfo);
+            string accountId = tupAccountInfo.SourceAccountId;
+            Assert.DoesNotThrow(() => client.Inquiries.GetBalances(docType, docNumber, accountId));
         }
     }
 }
